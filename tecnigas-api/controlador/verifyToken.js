514fcc22fd31cd8d7d863ff4f29e8609a0ff6a0a
Victor <victor.993_@hotmail.com>
2021-08-26 14:40:08 -0500

    Verificacion de usuario en verifyToken

--- tecnigas-api/controlador/verifyToken.js
+++ tecnigas-api/controlador/verifyToken.js
@@ -1,17 +1,26 @@
 const keys = require("../keys/keys.js");
 const password = keys.cryptoKey;
+const pool = require("./conexion");
 const jwt = require("jsonwebtoken");
 
-function verifyToken(req, res, next) {
+const verifyToken = async (req, res, next) => {
   const token = req.headers['token'];
-  if (token) {
+  console.log(req.headers)
+  if (!token) return res.status(401).send({ token: null, isAuth: false });
+
+  try {
     const deco = jwt.verify(token, password);
-    if (!deco)
-      return res.send({ token: null, isAuth: false });
+    const usuario_id = deco.id;
+    const response = await pool.query(
+      `select * from usuario natural join persona where usuario_id = ${usuario_id}`
+    );
+    const user = response.rows[0]
+    if (!user || user.nombre_pe !== deco.nombre_pe) return res.status(401).send({ token: null, isAuth: false });
+
+    next();
+  } catch (error) {
+    return res.status(401).send({ token: null, isAuth: false });
   }
-  next();
 }
 
-module.exports = {
-  verifyToken
-};
+module.exports = verifyToken;
