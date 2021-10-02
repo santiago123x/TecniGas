122a76491db1a316237ffa7b6f930850497a49a0
Andrés Felipe Moreno Delgado <andres.moreno.delgado@correounivalle.edu.co>
2021-10-02 10:25:10 -0500

    Avance en los gráficos
    Co-authored-by: davidneitor185 <davidneitor185@users.noreply.github.com>

---
+++ src/view/Informes/gaficoVentas.js
@@ -0,0 +1,59 @@
+import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer } from 'recharts';
+
+
+const graficoVentas = () => {
+    const data01 = [
+        { name: 'Group A', value: 400 },
+        { name: 'Group B', value: 300 },
+        { name: 'Group C', value: 300 },
+        { name: 'Group D', value: 200 },
+        { name: 'Group E', value: 278 },
+        { name: 'Group F', value: 189 },
+    ];
+
+    const data02 = [
+        { name: 'Group A', value: 2400 },
+        { name: 'Group B', value: 4567 },
+        { name: 'Group C', value: 1398 },
+        { name: 'Group D', value: 9800 },
+        { name: 'Group E', value: 3908 },
+        { name: 'Group F', value: 4800 },
+    ];
+
+    colorRamdo() {
+        var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        randomColor = "#" + ("000000" + randomColor).slice(-6);
+        return randomColor;
+    }
+
+
+
+    color() {
+        var color = Math.floor(Math.random() * 16777215).toString(16);
+        color = "#" + ("000000" + color).slice(-6);
+        color = `"background-color: ${color};"`;
+        console.log(color);
+        return color;
+    }
+
+    return (
+        <ResponsiveContainer width="100%" height="100%">
+            <PieChart width={400} height={400}>
+                <Pie
+                    dataKey="value"
+                    isAnimationActive={false}
+                    data={data01}
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={80}
+                    fill="#8884d8"
+                    label
+                />
+                <Pie dataKey="value" data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" />
+                <Tooltip />
+            </PieChart>
+        </ResponsiveContainer>
+    );
+};
+
+export default Informes;
--- src/view/Informes/informes.js
+++ src/view/Informes/informes.js
@@ -9,6 +9,7 @@ import Search from "../Componentes/Search";
 import "./InformesStyle.css";
 
 const Informes = () => {
+  //Declaración de estados y valores iniciales
   const auth = useAuth();
   const [informe, setInforme] = useState([]);
   const [valueInp, setValueInp] = useState("");
@@ -16,14 +17,14 @@ const Informes = () => {
   const [recarga, setRecarga] = useState(false);
   const { data, error, loading } = useAxios(url, recarga);
 
+  //Datos de la tabla
   const title = ["Código Factura", "Fecha Venta", "Subtotal", "Total", "Total Iva"];
   const titleDetails = ["Nombre Prod", "Código Producto", "Categoria", "Descuento", "Cantidad Vendida", "Precio Unitario" , "Total"];
   const detalleDos = ["Comprador", "Tipo", "Usuario Vendedor", "Recibido", "Cambio", "Observación", "Estado"];
-  let datico = [{}];
 
+  //Funciones
   const formateaFecha = () => {
       let opciones = { year: 'numeric', month: 'short', day: 'numeric' };
-      datico.length = 0;
       let rowsTempo=[];
     for(let i = 0; i<data.length; i++){
       let fecha = new Date(data[i].fecha_ve)
@@ -65,6 +66,7 @@ useEffect(()=>{
 /*const direcciona = () =>{
   window.location="/crea_devolucion";
 };*/
+
   return (
       <div className = "contteiner2">
       <div className = "containerList2">
@@ -101,123 +103,43 @@ useEffect(()=>{
                   />
                 )}
             </div>
+            <div className = "graficos">
+              <div className = "tituloGra">
+                <h3>Gráficos</h3>
+              </div>
+              <div className = "btnsGra">
+              <Button 
+                      size = "medium"
+                      variant ="contained"
+                      color ="primary"
+                      type = "button"
+                      //onClick={}
+                    >
+                      Ventas Por Producto
+                </Button>
+                <Button 
+                      size = "medium"
+                      variant ="contained"
+                      color ="primary"
+                      type = "button"
+                      //onClick={}
+                    >
+                      Ventas Por Mes
+                </Button>
+                <Button 
+                      size = "medium"
+                      variant ="contained"
+                      color ="primary"
+                      type = "button"
+                      //onClick={}
+                    >
+                      Ventas Por Año
+                </Button>
+              </div>
+            </div>
           </div>
       </div>
     );              
 };
 
 export default Informes;
-/*
-import React, { useState, useEffect } from "react";
-import Button from "@material-ui/core/Button";
-import useAxios from "../../Hooks/useAxios";
-import CollapsibleTable from "../../Componentes/Table/Table";
-import Loading from "../../Componentes/Loading/Loading";
-import Error404 from "../../Componentes/Error/Error";
-import Search from "../../Componentes/Search";
-import { useStyles } from "./FormDevUseStyles";
-import "./informesStyle";
-import useAuth from "../Hooks/useAuth";
-
-const Informes = () => {
-  const auth = useAuth();
-  const [info, setInfo] = useState([]);
-  const [valueInp, setValueInp] = useState("");
-  const [url, setUrl] = useState(`/listaVpersona/`);
-  const [recarga, setRecarga] = useState(false);
-  const { data, error, loading } = useAxios(url, recarga);
-
-  const title = ["Código Factura", "Atendido A", "Tipo", "Usuario", "Fecha Venta", "Subtotal", "Total", "Recibido",
-  "Cambio", "Estado", "Total Del Iva", "Observación"];
-  const titleDetails = ["Nombre Prod", "Código Producto", "Categoria", "Descuento", "Cantidad Vendida", "Precio Unitario" , "Total"];
-  let datico = [{}];
-  const classes = useStyles();
-
-  const formateaFecha = () => {
-      let opciones = { year: 'numeric', month: 'short', day: 'numeric' };
-      datico.length = 0;
-      let rowsTemp=[];
-    for(let i = 0; i<data.length; i++){
-      let fecha = new Date(data[i].fecha_ve)
-      .toLocaleDateString('es',opciones)
-      .replace(/ /g,'-')
-      .replace('.','')
-      .replace(/-([a-z])/, function (x) {return '-' + x[1].toUpperCase()});
-         const body = {
-          id_venta:data[i].id_venta,
-          nombre_pe:data[i].nombre_pe,
-          tipo_clpr:data[i].tipo_clpr,
-          nombre_usr:data[i].nombre_usr,
-          fecha_ve:fecha,
-          sub_total:data[i].sub_total,
-          total_ve:data[i].total_ve,
-          recibido:data[i].recibido,
-          cambio:data[i].cambio,
-          estado_ve:data[i].estado_ve,
-          total_iva:data[i].total_iva,
-          observacion_vta:data[i].observacion_vta,
-          nombre_pro:data[i].nombre_pro,
-          codigo_pro:data[i].codigo_pro,
-          nombre_catg:data[i].nombre_catg,
-          descuento:data[i].descuento,
-          cantidad_ven:data[i].cantidad_ven,
-          precio_ven:data[i].precio_ven,
-          total_ven:data[i].total_ven
-         };
-          rowsTemp.push(body);         
-       };
-       setInfo(rowsTemp);
-  };
-useEffect(()=>{
-  if(data){
-  formateaFecha();
-  }
-}, [data]);
-  
-/*const direcciona = () =>{
-  window.location="/crea_devolucion";
-};*/
-
-/*return (
-    <>
-      <div className = "contteiner">
-      <div className = "containerList">
-      <div className = "titulo_list"><h1> Listado De Ventas</h1></div>
-          <hr className="linea-h2" />
-            <div className = "contSearch"> 
-            <Search
-              valueInp={valueInp}
-              setValueInp={setValueInp}
-              titulo="Filtrar Devoluciones"
-              tooltip={`Tipos de Filtro:  Código Factura, Fecha Venta, Nombre Producto, Código Producto`}
-            />
-            </div>
-              <div className="lista-tabla">
-                {loading ? (
-                  <Loading />
-                ) : error ? (
-                  <Error404
-                    ancho={200}
-                    error="Se ha producido un problema, recargue la página."
-                  />
-                ) : 
-                    (
-                  <CollapsibleTable
-                    data={info}
-                    filtro={valueInp}
-                    titulos={title}
-                    titulosDetalles={titleDetails}
-                    tipo='info'
-                    categoria='info'
-                    recarga={recarga}
-                    setRecarga={setRecarga}
-                  />
-                )}
-            </div>
-          </div>
-      </div>
-    </>
-    );              
-};
-
-export default Informes;*/
